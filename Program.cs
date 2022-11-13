5d46bacd3f716a3d98d1b8d26281f1985130807b
RokiWielki <Cruck222@interia.pl>
2022-11-13 16:11:42 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Pelnomocnik
+{
+
+    public class User
+    {
+
+        private bool HasAdminPrivilages;
+
+        
+        public User(bool uprawnienia)
+        {
+            HasAdminPrivilages = uprawnienia;
+        }
+
+        public void MakeAdmin()
+        {
+            HasAdminPrivilages=true;
+        }
+
+        public bool IsAdmin()
+        {
+            HasAdminPrivilages = false;
+        }
+
+    }
+
+    public interface Information
+    {
+        public abstract void DisplayData();
+        public abstract void DisplayRestrictedData();
+    }
+
+    public class Database : Information
+    {
+
+        private Dictionary<string, double> Map;
+
+        public Database()
+        {
+            // stworzenie bazy użytkowników
+            // i uzupełnienie wartości
+        }
+
+        // wyświetlenie listy użytkowników
+
+        // wyświetlenie ujawniające zarobki
+
+    }
+
+    public class DatabaseGuard : Information
+    {
+
+        private Database DB;
+        private User user;
+
+        public DatabaseGuard(User u)
+        {
+            // stworzenie obiektu DB i przypisanie do pola
+            // u? pewnie pole ;)
+        }
+
+        public void DisplayData()
+        {
+            DB.DisplayData();
+        }
+
+        public void DisplayRestrictedData()
+        {
+            // sprawdzenie uprawnień i odpowienie działanie
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var Zbyszek = new User(false);
+            var db = new DatabaseGuard(Zbyszek);
+
+            db.DisplayData();
+
+            Console.WriteLine("---------------------------------------------------------");
+
+            db.DisplayRestrictedData();
+
+            Console.WriteLine("---------------------------------------------------------");
+
+            Zbyszek.MakeAdmin();
+            db.DisplayRestrictedData();
+
+        }
+    }
+
+}
